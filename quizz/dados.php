2fe2c8725ae61b6e619f3e0ba8aceec8cd4e12d6
Markoloko70 <102753392+Markoloko70@users.noreply.github.com>
2022-07-10 12:02:31 -0300

    Add files via upload

---
+++ quizz/dados.php
@@ -0,0 +1,7 @@
+<?php 
+	$dados = [
+		'titulo'=>'Gota do Himalaia Gota do Himalaia',
+		'descricao'=>'Potência, Controle e Tamanho. Seu Pênis do Jeito que Você Sempre Quis, Descubra o poder do segredo do Himalaia',
+		'link'=>'link do formulario aqui'
+	];
+?>
