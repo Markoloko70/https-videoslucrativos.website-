9ee42baf930256e54b8d19176f5c1efc4167af54
Markoloko70 <102753392+Markoloko70@users.noreply.github.com>
2022-07-12 14:31:47 -0300

    Delete dados.php

--- quizz/dados.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$dados = [
-		'titulo'=>'Gota do Himalaia Gota do Himalaia',
-		'descricao'=>'Potência, Controle e Tamanho. Seu Pênis do Jeito que Você Sempre Quis, Descubra o poder do segredo do Himalaia',
-		'link'=>'link do formulario aqui'
-	];
-?>
